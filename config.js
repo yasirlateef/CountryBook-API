11306359aa731d96a54a7c0f6a63fcb84642e3fc
yasir-lateef <yasirlateef112@gmail.com>
2016-05-24 23:32:28 +0500

    Deleted

--- config.js
+++
@@ -1,9 +0,0 @@
-var countriesList = require('./data/countries.json'),
-		regionsList = require('./data/regions.json'),
-		isocodeNames = require('./data/isocodes.json');
-
-module.exports = {
-	countries : countriesList,
-	regions : regionsList,
-	isocodes : isocodeNames
-}
